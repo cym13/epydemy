778b69547b299fd461cba0049f177a74875c079a
Cédric Picard <mystiro@gmail.com>
2014-05-05 22:17:07 +0200

    Added core multi-world functions

--- multi-world.py
+++ multi-world.py
@@ -52,10 +52,34 @@ class MultiWorld:
         self.destroyed = { x : 0 for x in zip(viruses, country) }
         self.protected = { x : 0 for x in zip(viruses, country) }
 
+        for each in zip(viruses, first_countries):
+            self.infected[each] = 1
+            self.viruses[each[0]].target = each[1]
+
+
     def step(self):
         """
         Perform one turn of spreading
         """
+        self.virus.age += 1
+
+        for v in self.viruses:
+            inf_r  = v.spread * 0.001
+            dest_r = v.danger * 0.001
+            prot_r = v.detect * 0.001
+            rent_r = v.rentab
+
+            if inf_r < 0:
+                inf_r = 0
+            if dest_r < 0:
+                dest_r = 0
+            if prot_r < 0:
+                prot_r = 0
+            if rent_r < 0:
+                rent_r = 0
+
+            for c in countries:
+                self.spread(v, c, inf_r, dest_r, prot_r)
 
 
     def money(self, country, rent_r):
@@ -65,12 +89,61 @@ class MultiWorld:
                          * countries[country]["money"])
 
 
-    # To change
+    # This may be simplified
     def spread(self, virus, country, inf_r, dest_r, prot_r):
         """
         Core of the game mechanics
         Manages the evolution of the infection in a country
         """
+        target  = virus.target
+
+        sane      = self.sane[virus, country]
+        infected  = self.sane[virus, country]
+        destroyed = self.sane[virus, country]
+        protected = self.sane[virus, country]
+
+        # Asymptotic limit counter-measure
+        p_sane      = sane + 1000
+        p_infected  = infected
+        p_destroyed = destroyed
+        p_protected = protected
+        computers = countries["computers"]
+
+        # Add some randomness to the values
+        inf_r  *= (random.randint(1, 10) / 30)
+        dest_r *= (random.randint(1, 10) / 30)
+        prot_r *= (random.randint(1, 10) / 30)
+
+        if target == None:
+            pass
+        elif target == country:
+            inf_r *= 2.00
+        else:
+            inf_r *= 0.10
+
+        sane -= round(inf_r * p_sane)
+        if sane < 0:
+            sane = 0
+
+        infected += round(inf_r  * p_sane)
+        infected -= round(dest_r * p_infected)
+        infected -= round(prot_r * p_infected)
+        if infected > computers:
+            infected = computers
+
+        destroyed += round(dest_r * infected)
+        if destroyed > computers:
+            destroyed = computers
+
+        protected += round(prot_r * infected)
+        if protected > computers:
+            protected = computers
+
+        self.sane[virus, country]      = sane
+        self.infected[virus, country]  = infected
+        self.protected[virus, country] = protected
+        self.destroyed[virus, country] = destroyed
+
 
     # To change
     def upgrade(virus, immunity_rate, country_lst=None):
